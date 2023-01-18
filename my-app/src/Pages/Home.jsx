060aa982458c7403e93080734142f06a0345ae42
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-01-18 10:07:36 +0530

    added first day

--- my-app/src/Pages/Home.jsx
+++ my-app/src/Pages/Home.jsx
@@ -5,9 +5,28 @@ export default function Home() {
   return (
     <div>
       <div className='homeFirstDiv' >
-        <p>FREE SHIPPINGON ORDERS OVER $75</p>
-        <p>FREE RETURNSEVERYDAY</p>
+        <p><span>FREE SHIPPINGON</span> ORDERS OVER $75</p>
+        <p>|</p>
+        <p><span>FREE RETURNS</span> EVERYDAY</p>
       </div>
+      <div className='winter-clearnce' >
+        <img src="https://clarks.scene7.com/is/image/Pangaea2Build/BANWKS50and51MBootsSaleDesktopBanner1500x500px?fmt=webp&wid=1500" alt="" />
+        <div className='container' >
+          <p>WINTER CLEARANCE</p>
+          <p>SAVE UP TO</p>
+          <p>50% OFF*</p>
+          <p>TAKE AN EXTRA 30% OFF MARKDOWNS</p>
+          <p>USE CODE: EXTRA</p>
+          <div>
+            <p> Shop Womens</p>
+            <p>Shop Mens</p>
+            <p>Shop Kids</p>
+            <p>Shop All</p>
+          </div>
+          <p>For more details see the terms & conditions</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
